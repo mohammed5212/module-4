25dd483a5cbc9739062df7e5af52c251716e49f4
mohammed5212 <mohammedchpktr@gmail.com>
2025-08-15 19:47:53 +0530

    modified:   controllers/authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./mo");
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const password_hash = await bcrypt.password_hash(password, salt);
+    const user = await User.create({ username, email, password_hash, role });
+    return res.status(201).json({
+      id: User._id,
+      username: User.username,
+      email: User.email,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "server errorduring registration" });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Misssing fields" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid cridentials" });
+    }
+    const match = await bcrypt.compare(password, User.password_hash);
+    if (!match) {
+      return res.status(401).json({ message: "Invalid cridentials" });
+    }
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || "7d" }
+    );
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "server error during login" });
+  }
+};
+module.exports = (register, login);
